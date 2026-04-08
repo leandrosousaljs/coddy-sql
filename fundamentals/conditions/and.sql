b32ba43f71a76b727854f82d6da30f04ed7bac7c
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-04-08 12:04:44 -0300

    add and challenge

---
+++ fundamentals/conditions/and.sql
@@ -0,0 +1,8 @@
+/* Challenge
+
+Fetch all of the people from the people table who are between the ages of 20 and 28 (including 20 and 28). */
+
+SELECT * 
+FROM people
+-- Write your code below
+WHERE age >= 20 AND age <= 28
