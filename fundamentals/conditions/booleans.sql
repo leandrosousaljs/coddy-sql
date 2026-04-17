054dc2881e7651283c24c46669e1cde510b9748f
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-04-17 14:00:54 -0300

    add booleans challenge

---
+++ fundamentals/conditions/booleans.sql
@@ -0,0 +1,10 @@
+/* Challenge
+
+Available tables and columns:
+objects: id, colorful
+
+Fetch all of the colorful objects. Instead of writing colorful = 1 try to use the TRUE keyword. */
+
+SELECT *
+FROM objects
+WHERE colorful IS TRUE
