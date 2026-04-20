57c68b006be18316621fd6b1332d7d16c3d0fecb
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-04-20 11:55:03 -0300

    add sort P1 challenge

---
+++ fundamentals/specificReturnFormat/sortP1.sql
@@ -0,0 +1,10 @@
+/* Challenge
+
+Available tables and columns:
+feathers: id, weight
+
+Return all of the ids after ordering them by the weight in descending order */
+
+SELECT id
+FROM feathers
+ORDER BY weight DESC
