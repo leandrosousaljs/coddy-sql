c264cb2692052d1f375cd1b71c6faf91f13a6b08
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-04-26 09:38:57 -0300

    add between challenge

---
+++ fundamentals/moreKeywords/between.sql
@@ -0,0 +1,10 @@
+/* Challenge
+
+Available tables and columns:
+data: value
+
+Fetch all of the records where the value column is between 7 and 13 */
+
+SELECT *
+FROM data
+WHERE value BETWEEN 7 AND 13
